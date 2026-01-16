4affacc589e00c9b63f489bbb876ed5b44ed19df
Marlou Dev <web@empireonegroup.com>
2026-01-17 01:40:23 +0800

    remove unnessary

--- routes/web.php
+++ routes/web.php
@@ -9,64 +9,64 @@ Route::get('/', function () {
     return Inertia::render('homepage/page');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 
-Route::prefix('administrator')->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('administrator/dashboard/page');
-    });
-    Route::get('/subscribers', function () {
-        return Inertia::render('administrator/subscribers/page');
-    });
+// Route::prefix('administrator')->group(function () {
+//     Route::get('/dashboard', function () {
+//         return Inertia::render('administrator/dashboard/page');
+//     });
+//     Route::get('/subscribers', function () {
+//         return Inertia::render('administrator/subscribers/page');
+//     });
 
-    Route::get('/users', function () {
-        return Inertia::render('administrator/users/page');
-    });
-    Route::get('/settings', function () {
-        return Inertia::render('administrator/settings/page');
-    });
-});
+//     Route::get('/users', function () {
+//         return Inertia::render('administrator/users/page');
+//     });
+//     Route::get('/settings', function () {
+//         return Inertia::render('administrator/settings/page');
+//     });
+// });
 
-Route::prefix('account')->group(function () {
-    Route::prefix('pos')->group(function () {
-        Route::get('/dashboard', function () {
-            return Inertia::render('account/pos/dashboard/page');
-        });
-        Route::get('/cashiering', function () {
-            return Inertia::render('account/pos/cashiering/page');
-        });
-        Route::get('/pos', function () {
-            return Inertia::render('account/pos/pos/page');
-        });
-        Route::get('/products', function () {
-            return Inertia::render('account/pos/products/page');
-        });
-        Route::get('/stack_movements', function () {
-            return Inertia::render('account/pos/stack_movements/page');
-        });
-        Route::get('/purchases', function () {
-            return Inertia::render('account/pos/purchases/page');
-        });
-        Route::get('/suppliers', function () {
-            return Inertia::render('account/pos/suppliers/page');
-        });
-        Route::get('/customers', function () {
-            return Inertia::render('account/pos/customers/page');
-        });
-        Route::get('/cash_register', function () {
-            return Inertia::render('account/pos/cash_register/page');
-        });
-        Route::get('/reports', function () {
-            return Inertia::render('account/pos/reports/page');
-        });
-        Route::get('/settings', function () {
-            return Inertia::render('account/pos/settings/page');
-        });
-    });
-});
+// Route::prefix('account')->group(function () {
+//     Route::prefix('pos')->group(function () {
+//         Route::get('/dashboard', function () {
+//             return Inertia::render('account/pos/dashboard/page');
+//         });
+//         Route::get('/cashiering', function () {
+//             return Inertia::render('account/pos/cashiering/page');
+//         });
+//         Route::get('/pos', function () {
+//             return Inertia::render('account/pos/pos/page');
+//         });
+//         Route::get('/products', function () {
+//             return Inertia::render('account/pos/products/page');
+//         });
+//         Route::get('/stack_movements', function () {
+//             return Inertia::render('account/pos/stack_movements/page');
+//         });
+//         Route::get('/purchases', function () {
+//             return Inertia::render('account/pos/purchases/page');
+//         });
+//         Route::get('/suppliers', function () {
+//             return Inertia::render('account/pos/suppliers/page');
+//         });
+//         Route::get('/customers', function () {
+//             return Inertia::render('account/pos/customers/page');
+//         });
+//         Route::get('/cash_register', function () {
+//             return Inertia::render('account/pos/cash_register/page');
+//         });
+//         Route::get('/reports', function () {
+//             return Inertia::render('account/pos/reports/page');
+//         });
+//         Route::get('/settings', function () {
+//             return Inertia::render('account/pos/settings/page');
+//         });
+//     });
+// });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
