d9f349dcc0ee968c907ec7b6a832bd2adbb57577
Marlou Dev <web@empireonegroup.com>
2026-01-17 01:41:28 +0800

    remove api

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\POS\PosCashRegisterController;
-use App\Http\Controllers\API\POS\PosCashTransactionController;
-use App\Http\Controllers\API\POS\PosCategoryController;
-use App\Http\Controllers\API\POS\PosCustomerController;
-use App\Http\Controllers\API\POS\PosItemController;
-use App\Http\Controllers\API\POS\PosProductController;
-use App\Http\Controllers\API\POS\PosProductStockController;
-use App\Http\Controllers\API\POS\PosPurchaseController;
-use App\Http\Controllers\API\POS\PosPurchaseItemController;
-use App\Http\Controllers\API\POS\PosSaleController;
-use App\Http\Controllers\API\POS\PosSalesItemController;
-use App\Http\Controllers\API\POS\PosStockMovementController;
-use App\Http\Controllers\API\POS\PosUnitController;
-use App\Http\Controllers\PosSupplierController;
+// use App\Http\Controllers\API\POS\PosCashRegisterController;
+// use App\Http\Controllers\API\POS\PosCashTransactionController;
+// use App\Http\Controllers\API\POS\PosCategoryController;
+// use App\Http\Controllers\API\POS\PosCustomerController;
+// use App\Http\Controllers\API\POS\PosItemController;
+// use App\Http\Controllers\API\POS\PosProductController;
+// use App\Http\Controllers\API\POS\PosProductStockController;
+// use App\Http\Controllers\API\POS\PosPurchaseController;
+// use App\Http\Controllers\API\POS\PosPurchaseItemController;
+// use App\Http\Controllers\API\POS\PosSaleController;
+// use App\Http\Controllers\API\POS\PosSalesItemController;
+// use App\Http\Controllers\API\POS\PosStockMovementController;
+// use App\Http\Controllers\API\POS\PosUnitController;
+// use App\Http\Controllers\PosSupplierController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -24,18 +24,18 @@ Route::get('/user', function (Request $request) {
 
 
 // Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('pos-registers', PosCashRegisterController::class);
-    Route::post('pos-registers/{pos_register}/transaction', [PosCashRegisterController::class, 'addTransaction']);
-    Route::apiResource('pos-transactions', PosCashTransactionController::class);
-    Route::apiResource('pos-categories', PosCategoryController::class);
-    Route::apiResource('pos-customers', PosCustomerController::class);
-    Route::apiResource('pos-products', PosProductController::class);
-    Route::apiResource('pos-product-stocks', PosProductStockController::class);
-    Route::apiResource('pos-purchases', PosPurchaseController::class);
-    Route::apiResource('pos-sales', PosSaleController::class);
-    Route::apiResource('pos-sale-items', PosSalesItemController::class);
-    Route::apiResource('pos-stock-movements', PosStockMovementController::class);
-    Route::apiResource('pos-suppliers', PosSupplierController::class);
-    Route::apiResource('pos-units', PosUnitController::class);
-    Route::apiResource('pos-purchase-items', PosPurchaseItemController::class);
+    // Route::apiResource('pos-registers', PosCashRegisterController::class);
+    // Route::post('pos-registers/{pos_register}/transaction', [PosCashRegisterController::class, 'addTransaction']);
+    // Route::apiResource('pos-transactions', PosCashTransactionController::class);
+    // Route::apiResource('pos-categories', PosCategoryController::class);
+    // Route::apiResource('pos-customers', PosCustomerController::class);
+    // Route::apiResource('pos-products', PosProductController::class);
+    // Route::apiResource('pos-product-stocks', PosProductStockController::class);
+    // Route::apiResource('pos-purchases', PosPurchaseController::class);
+    // Route::apiResource('pos-sales', PosSaleController::class);
+    // Route::apiResource('pos-sale-items', PosSalesItemController::class);
+    // Route::apiResource('pos-stock-movements', PosStockMovementController::class);
+    // Route::apiResource('pos-suppliers', PosSupplierController::class);
+    // Route::apiResource('pos-units', PosUnitController::class);
+    // Route::apiResource('pos-purchase-items', PosPurchaseItemController::class);
 // });
